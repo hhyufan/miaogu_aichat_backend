1627df77c77ddf40266d9ecbddefe3cebb2f25c7
hhyufan <1838248655@qq.com>
2025-01-31 18:50:18 +0800

    fix: 修复密码验证正则表达式特殊字符包含不全的问题

--- src/main/kotlin/com/miaogu/handler/PasswordStrengthValidationHandler.kt
+++ src/main/kotlin/com/miaogu/handler/PasswordStrengthValidationHandler.kt
@@ -12,7 +12,7 @@ class PasswordStrengthValidationHandler : UserValidationHandler() {
 
     override fun handle(user: User) {
         // 定义密码强度的正则表达式
-        val passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@!%*#?&])[A-Za-z\\d$@!%*#?&]{8,24}$".toRegex()
+        val passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_])\\S{8,24}$".toRegex()
         println(user.password)
         // 使用正则表达式验证密码
         user.password?.let {
