859daddf84c0e9c180591865d2222cd6a26d8a4b
hhyufan <1838248655@qq.com>
2025-02-14 20:04:19 +0800

    refactor(password): 删除后端的密码强度验证，转交给前端处理

--- src/main/kotlin/com/miaogu/chain/UserValidationChain.kt
+++ src/main/kotlin/com/miaogu/chain/UserValidationChain.kt
@@ -3,7 +3,6 @@ package com.miaogu.chain
 import com.miaogu.entity.User
 import com.miaogu.handler.EmailDuplicateCheckHandler
 import com.miaogu.handler.EmailFormatValidationHandler
-import com.miaogu.handler.PasswordStrengthValidationHandler
 import com.miaogu.handler.UsernameDuplicateCheckHandler
 import com.miaogu.mapper.UserMapper
 
@@ -11,18 +10,17 @@ class UserValidationChain(target: UserMapper) {
     private val usernameDuplicateCheck = UsernameDuplicateCheckHandler(target)
     private val emailDuplicateCheckHandler = EmailDuplicateCheckHandler(target)
     private val emailCheck = EmailFormatValidationHandler()
-    private val passwordStrengthCheck = PasswordStrengthValidationHandler()
 
     init {
         // 建立责任链 emailCheck: EmailFormatValidationHandler
         usernameDuplicateCheck.setNext(emailDuplicateCheckHandler).setNext(emailCheck)
-            .setNext(passwordStrengthCheck)
+
     }
 
     fun validate(user: User): String? {
 
         usernameDuplicateCheck.handle(user)
-        return usernameDuplicateCheck.currentError ?: emailDuplicateCheckHandler.currentError ?: emailCheck.currentError ?: passwordStrengthCheck.currentError
+        return usernameDuplicateCheck.currentError ?: emailDuplicateCheckHandler.currentError ?: emailCheck.currentError
             // 如果没有错误，返回 null
     }
 }
