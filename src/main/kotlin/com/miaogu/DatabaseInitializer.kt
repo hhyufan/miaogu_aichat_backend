e6e940e0d088d14ce253cdc2b852a65b2847bf2a
YukiIsait <yukiisait@outlook.jp>
2025-03-06 18:47:04 +0800

    feat: 更灵活的初始化SQL执行方式

--- src/main/kotlin/com/miaogu/DatabaseInitializer.kt
+++ src/main/kotlin/com/miaogu/DatabaseInitializer.kt
@@ -1,7 +1,10 @@
 package com.miaogu
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
+import org.springframework.core.io.AbstractResource
 import org.springframework.core.io.ClassPathResource
+import org.springframework.core.io.FileSystemResource
 import org.springframework.core.io.support.EncodedResource
 import org.springframework.jdbc.core.ConnectionCallback
 import org.springframework.jdbc.core.JdbcTemplate
@@ -10,14 +13,23 @@ import org.springframework.stereotype.Component
 
 @Component
 class DatabaseInitializer(private val jdbcTemplate: JdbcTemplate) : CommandLineRunner {
+    @Value("\${database.init-script-file:init_database.sql}")
+    private lateinit var databaseInitScriptFile: String
 
     override fun run(vararg args: String?) {
         initializeDatabase()
     }
 
     private fun initializeDatabase() {
+        var scriptResource: AbstractResource = FileSystemResource(databaseInitScriptFile)
+        if (!scriptResource.exists()) {
+            scriptResource = ClassPathResource(databaseInitScriptFile)
+            if (!scriptResource.exists()) {
+                return
+            }
+        }
         jdbcTemplate.execute(ConnectionCallback {
-            ScriptUtils.executeSqlScript(it, EncodedResource(ClassPathResource("sql/init_data.sql"), "utf-8"))
+            ScriptUtils.executeSqlScript(it, EncodedResource(scriptResource, "utf-8"))
         })
     }
 }
